c8c4197a78cb743a61425aca1849a94c04abe9df
Ivan Fomin <ivfmn1@gmail.com>
2026-06-12 00:18:56 +0400

    change base in vite config

--- landing-app/vite.config.ts
+++ landing-app/vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), reactRouter(), tailwindcss()],
+  base: "/business-dev-page/",
   // resolve: {
   //   alias: {
   //     "~": path.resolve(__dirname, "app"),
